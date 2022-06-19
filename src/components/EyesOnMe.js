e895089d530e473f3e7cb66c36198501a81e658f
Jeremy Platt <jplatt4427@gmail.com>
2022-06-19 14:03:30 -0700

    Completed Assignmemnt

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,22 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+
+
+function EyesOnMe() {
+    function doStuff(event) {
+      
+    }
+
+    function focus(event){
+        console.log('Good!');
+    }
+    function blur(event){
+        console.log('Hey! Eyes on me!');
+    }
+    
+  
+    return <button onClick={doStuff} onFocus = {focus} onBlur = {blur} >Eyes on me</button>;
+  }
+  
+export default EyesOnMe  
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,22 @@
 // Code Keypad Component Here
+import React from "react";
+
+function Keypad() {
+    function handleSubmit(event) {
+      event.preventDefault();
+      
+    }
+    function handleChange(){
+        console.log("Entering password...");
+    }
+  
+    return (
+      <form onSubmit={handleSubmit}>
+        <input type="password" name="password" onChange={handleChange} placeholder="Enter password..." />
+      </form>
+    );
+  }
+
+  export default Keypad;
+  
+  
